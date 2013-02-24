98db79fb4f63684a42ce9923cf29a77cf8b995f5
boss94 <cyberboss94@gmail.com>
2013-02-24 14:59:29 +0100

    Pages avec Layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,18 @@
+# -*- encoding : utf-8 -*-
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+
+    it "a le bon titre" do
+      get 'home'
+      response.should have_selector("title", :content => "| Accueil")
     end
   end
 
@@ -14,6 +21,24 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "a le bon titre" do
+      get 'contact'
+      response.should have_selector("title", :content => "| Contact")
+    end
   end
+
+  describe "GET 'about'" do
+    it "devrait reussir" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "a le bon titre" do
+      get 'about'
+      response.should have_selector("title", :content => "| À Propos")
+    end
+  end
+
 
 end
